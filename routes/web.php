b04f06992893c9bfdb280b8af9510206146ed01e
RAVE RYAN LABERGUE BELTRAN <2202088@cc.usls.edu>
2025-01-30 10:57:45 +0800

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', 'App\Http\Controllers\LoginController@index');
+
+Route::get('/inventory', 'App\Http\Controllers\InventoryController@index');
